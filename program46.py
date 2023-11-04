c5e8060339d9d821016c6a698131426a1dcc5eb9
Akhil Rock Babu <146969874+akhilrockbabu@users.noreply.github.com>
2023-11-04 18:18:08 +0530

    Update program46.py

--- program46.py
+++ program46.py
@@ -1,21 +1,22 @@
 '''Generate a list of four digit numbers in a given range with all their digits even and the number is a perfect square. '''
 
+def sqrt_even_num(n):
+    l=[]
+    if n<=9999 and n>999:
+        for i in range(1000,n+1):
+            if math.sqrt(i).is_integer():
+                s=str(i)
+                if s[0]!='0' and s[1]!='0' and s[2]!='0' and s[3]!='0':
+                    if int(s[0])%2==0 and int(s[1])%2==0 and int(s[2])%2==0 and int(s[3])%2==0:
+                        l.append(s)
+    else:
+        print('please enter a four digit number')
+    return l
 
 import math
 n=int(input("enter a four dgitit number"))
-l=[]
-if n<=9999:
-    for i in range(1000,n+1):
-        if math.sqrt(i).is_integer():
-            s=str(i)
-            
-            l.append(i)
-print(l)
+print(sqrt_even_num(n))
 
 
 
-
-
-        
-'''s=str(i)
-if s[0]%2==0 and s[1]%2==0 and s[2]%2==0 and s[3]%2==0 :'''
+    
