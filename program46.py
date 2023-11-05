ede63689f04a4549a1475bd5fdfa3eab752dc5d4
Akhil Rock Babu <146969874+akhilrockbabu@users.noreply.github.com>
2023-11-05 14:21:47 +0530

    Update program46.py

--- program46.py
+++ program46.py
@@ -1,10 +1,10 @@
 '''Generate a list of four digit numbers in a given range with all their digits even and the number is a perfect square. '''
 
 import math
-def sqrt_even_num(n):
+def sqrt_even_num(n1,n2):
     l=[]
-    if n<=9999 and n>999:
-        for i in range(1000,n+1):
+    if n1>999 and n2<=9999:
+        for i in range(n1,n2+1):
             if math.sqrt(i).is_integer():
                 s=str(i)
                 if s[0]!='0' and s[1]!='0' and s[2]!='0' and s[3]!='0':
@@ -15,10 +15,12 @@ def sqrt_even_num(n):
     return l
 
 
-n=int(input("enter a four dgitit number"))
-print(sqrt_even_num(n))
+n1=int(input("enter a four digit lower limit"))
+n2=int(input("enter a four digit upper limit"))
+print(sqrt_even_num(n1,n2))
 
 
-'''enter a four dgitit number9999
+'''enter a four digit lower limit2000
+enter a four digit upper limit9000
 ['4624', '8464']'''
     
