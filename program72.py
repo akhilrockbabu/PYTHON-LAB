52fdefafe168604973a207b9e55151cc2b682253
akhilrockbabu <akhilrockbabu@gmail.com>
2023-12-14 21:25:39 +0530

    'update'

--- program72.py
+++ program72.py
@@ -10,9 +10,10 @@ insert.writeheader()
 rows=[{'name':'akhil','age':'21','gender':'male'},{'name':'dona','age':'20','gender':'female'}] 
 insert.writerows(rows)
 
-read=csv.DictReader(f)
-for lines in read:
-    if lines:
-        print(lines[name],'\t',lines[age],'\t',lines[gender])
-
+f1=open('prgrm72.csv',mode='r')
+readit=csv.DictReader(f1)
+for lines in readit:
+    if readit:
+        print(lines[0],'\t',lines[1],'\t',lines[2])
+f1.close()
 f.close()
